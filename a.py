681ceef1ae0f6c9b599b066738f4b4c4dc758af8
Luan-fss <luan.peruibe2@hotmail.com>
2022-08-10 16:37:24 -0300

    Modificado arquivos para versão mais recente

--- a.py
+++ a.py
@@ -1,53 +1,87 @@
-import pygame
+import pygame, random
 from pygame.math import Vector2
 
+# Criado movimento da Snake
+# Colocado tempo para esse movimento nao ser feito rapido no loop = Criar um timer
+# Colocada class Main para organização ...
+class Snake:
+    COLOR = ((255,255,255))
+    def __init__(self):
+        self.body = [Vector2(1,10), Vector2(2,10), Vector2(3,10)]
+        self.direction = Vector2(1,0)
+
+    def draw_snake(self):
+        for block in self.body:
+            pygame.draw.rect(screen, self.COLOR, (block.x * CELL_SIZE, block.y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
+    def move_snake(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0, body_copy[0] + self.direction)
+        self.body = body_copy[:]
+        print(self.body)
+
+class Fruit:
+    COLOR = ((255,0,0))
+    def __init__(self):
+        self.x = random.randint(0, CELL_NUMBER - 1)
+        self.y = random.randint(0, CELL_NUMBER - 1)
+        self.pos = Vector2(self.x, self.y)
+
+    def draw_fruit(self):
+        pygame.draw.rect(screen, self.COLOR, (self.pos.x*CELL_SIZE, self.pos.y*CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
+class Main:
+    def __init__(self):
+        self.snake = Snake()
+        self.fruit = Fruit()
+
+    def update(self):
+        self.snake.move_snake()
+
+    def draw_elemments(self):
+        self.snake.draw_snake()
+        self.fruit.draw_fruit()
+        
 pygame.init()
 CELL_NUMBER = 20
 CELL_SIZE = 40
+WIDTH, HEIGHT = CELL_NUMBER*CELL_SIZE, CELL_NUMBER*CELL_SIZE
 FPS = 60
-screen = pygame.display.set_mode((CELL_NUMBER*CELL_SIZE, CELL_SIZE*CELL_NUMBER))
-pygame.display.set_caption('TESTE')
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Snake')
 clock = pygame.time.Clock()
+# Adicionar TIMER em um evento costumizado
+SCREEN_UPDATE = pygame.USEREVENT # Criando um event custom pra adicionar ao Loop
+pygame.time.set_timer(SCREEN_UPDATE, 150)
 
-
-class Quad_opponent:
-    COLOR = ('red')
-    def __init__(self):
-        self.x = 1
-        self.y = 1
-        self.pos = Vector2(self.x, self.y)
-
-    def draw(self):
-        pygame.draw.rect(screen, self.COLOR, (self.pos.x * CELL_SIZE, self.pos.y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
-
-class Quad_player:
-    COLOR = ('blue')
-    def __init__(self):
-        self.x = CELL_NUMBER - 2
-        self.y = CELL_NUMBER - 2
-        self.pos = Vector2(self.x, self.y)
-
-    def draw(self):
-        pygame.draw.rect(screen, self.COLOR, (self.pos.x * CELL_SIZE, self.pos.y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+main_game = Main()
 
 def main():
     run = True
-    quad_player = Quad_player()
-    quad_opponent = Quad_opponent()
 
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+            # Timer
+            if event.type == SCREEN_UPDATE:
+                main_game.update()
+            # Keys to control snake
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    main_game.snake.direction = Vector2(0,-1)
+                if event.key == pygame.K_DOWN:
+                    main_game.snake.direction = Vector2(0,1)
+                if event.key == pygame.K_LEFT:
+                    main_game.snake.direction = Vector2(-1,0)
+                if event.key == pygame.K_RIGHT:
+                    main_game.snake.direction = Vector2(1,0)
 
-        screen.fill('Black')
-        quad_player.draw()
-        quad_opponent.draw()
-        pygame.draw.rect(screen, ('White'), (0 ,17*CELL_SIZE-5, 800, 5))
-        pygame.draw.rect(screen, ('White'), (0 ,3*CELL_SIZE, 800, 5))
-        pygame.draw.aaline(screen,(255,0,0), (0, 17*CELL_SIZE-5), (800, 3*CELL_SIZE+3), blend=1)
-
+        # Draw
+        screen.fill((0,0,0))
+        main_game.draw_elemments()
+        # Setup DISPLAY
         pygame.display.update()
         clock.tick(FPS)
 
--- teste.py
+++ teste.py
@@ -1,41 +1,39 @@
-# Escopo = Create settings > Import here
-# Create def main() for upgrade in screen and fixed window
-# Create classes > draw > move... Pass all of them to Loop in main()
 import pygame, random
-from settings import *
 from pygame.math import Vector2
 
-class Snake():
-    COLOR = ((183,111,122))
+# Criado movimento da Snake
+# Colocado tempo para esse movimento nao ser feito rapido no loop = Criar um timer
+# Colocada class Main para organização ...
+class Snake:
+    COLOR = ((255,255,255))
     def __init__(self):
         self.body = [Vector2(5,10), Vector2(6,10), Vector2(7,10)]
         self.direction = Vector2(1,0)
 
     def draw_snake(self):
-        # Vai checar todos o vetores do body da snake... Ex: {5,10}, {6,10}, {7,10}... E vai criar os rect nas devidas coordenadas
         for block in self.body:
-            pygame.draw.rect(screen, self.COLOR,(block.x*CELL_SIZE, block.y*CELL_SIZE, CELL_SIZE, CELL_SIZE))
+            pygame.draw.rect(screen, self.COLOR, (block.x*CELL_SIZE, block.y*CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
-    def move_snake(self):    
-        body_copy = self.body[:-1] 
-        body_copy.insert(0,body_copy[0] + self.direction)
-        self.body = body_copy[:]   
+    def move_snake(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0, self.body[0]+self.direction)
+        self.body = body_copy
 
-class Fruit():
-    COLOR = ((122,166,114))
+class Fruit:
+    COLOR = ((255,0,0))
     def __init__(self):
         self.x = random.randint(0, CELL_NUMBER-1)
         self.y = random.randint(0, CELL_NUMBER-1)
-        self.pos = pygame.math.Vector2(self.x, self.y)
+        self.pos = Vector2(self.x, self.y)
 
     def draw_fruit(self):
-        pygame.draw.rect(screen, self.COLOR, (int(self.pos.x*CELL_SIZE), int(self.pos.y*CELL_SIZE), CELL_SIZE, CELL_SIZE))
+        pygame.draw.rect(screen, self.COLOR, (self.pos.x*CELL_SIZE, self.pos.y*CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
-class Main():
+class Main:
     def __init__(self):
         self.snake = Snake()
         self.fruit = Fruit()
-    
+
     def update(self):
         self.snake.move_snake()
 
@@ -43,9 +41,20 @@ class Main():
         self.snake.draw_snake()
         self.fruit.draw_fruit()
 
-main_game = Main()
-SCREEN_UPDATE = pygame.USEREVENT        
+pygame.init()
+CELL_NUMBER = 20
+CELL_SIZE = 40
+WIDTH, HEIGHT = CELL_NUMBER*CELL_SIZE, CELL_NUMBER*CELL_SIZE
+FPS = 60
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Snake')
+clock = pygame.time.Clock()
+
+# Setando Timer
+SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 150)
+
+main_game = Main()
 
 def main():
     run = True
@@ -76,7 +85,6 @@ def main():
         # Setup DISPLAY
         pygame.display.update()
         clock.tick(FPS)
-
 
 if __name__ == '__main__':
     main()
